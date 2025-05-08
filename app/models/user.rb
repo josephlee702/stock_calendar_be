03c7ad860d631d4b9cac142c606a23381d15b0b7
Joseph Lee <jhjlee702@gmail.com>
2025-05-08 15:35:27 -0600

    delete devise line since we don't have devise gem and add extend models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable,
+        #  :recoverable, :rememberable, :validatable
+  extend Devise::Models
   include DeviseTokenAuth::Concerns::User
 end
