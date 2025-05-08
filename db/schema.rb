b02ce5d573d743816198beb727de5a0fb84b9966
Joseph Lee <jhjlee702@gmail.com>
2025-05-08 15:43:03 -0600

    add trade model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_08_212734) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_08_214220) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "trades", force: :cascade do |t|
+    t.date "date"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -38,4 +45,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_08_212734) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
+
 end
