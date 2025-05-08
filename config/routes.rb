b3261c31e4897201d48a7d4208ba84d0095a0c03
Joseph Lee <jhjlee702@gmail.com>
2025-05-08 15:34:16 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  
+  get "up" => "rails/health#show", as: :rails_health_check
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: "auth"
       resources :users, only: [:index, :show]
-      end
     end
   end
-  get "up" => "rails/health#show", as: :rails_health_check
 end
