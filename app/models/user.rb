314391408cd8b6bd40b1f26a378031452f7eda1f
Joseph Lee <jhjlee702@gmail.com>
2025-05-12 16:58:34 -0600

    Add devise gem and builtin devise model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   has_many :trades, dependent: :destroy
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-        #  :recoverable, :rememberable, :validatable
+
   extend Devise::Models
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
