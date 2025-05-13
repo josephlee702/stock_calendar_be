266178adf4de6455eb7d4150b90a62bf5453cad0
Joseph Lee <jhjlee702@gmail.com>
2025-05-12 18:51:30 -0600

    adjust validations

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -7,5 +7,5 @@ class Trade < ApplicationRecord
   validates :trade_type, inclusion: { in: TRADE_TYPES }
   validates :option_type, inclusion: { in: OPTION_TYPES }, allow_nil: true
 
-  validates :symbol, :date, :profit, :trade_type, presence: true
+  validates :symbol, :date, :amount, :option_type, :notes, :was_disciplined, :trade_type, presence: true
 end
