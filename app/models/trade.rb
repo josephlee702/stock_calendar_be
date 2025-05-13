947763c6c0f838550885d21308d8ef2b48e7fb68
Joseph Lee <jhjlee702@gmail.com>
2025-05-12 18:45:14 -0600

    add validations for new fields

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,3 +1,11 @@
 class Trade < ApplicationRecord
   belongs_to :user
+
+  TRADE_TYPES = %w[Buy Sell]
+  OPTION_TYPES = %w[Call Put]
+
+  validates :trade_type, inclusion: { in: TRADE_TYPES }
+  validates :option_type, inclusion: { in: OPTION_TYPES }, allow_nil: true
+
+  validates :symbol, :date, :profit, :trade_type, presence: true
 end
