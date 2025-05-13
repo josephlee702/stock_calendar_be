fec522ae08dd97d2b078c536a4a0a71179a4e959
Joseph Lee <jhjlee702@gmail.com>
2025-05-12 18:51:42 -0600

    adjust seeds based on new Trade model

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,18 @@ User.destroy_all
 user1 = User.create(
     name: "Joseph Lee", 
     email: "jhjlee702@gmail.com", 
-    password: "ffffff", 
-    password_confirmation: "ffffff"
+    password: "123456", 
+    password_confirmation: "123456"
   )
 
 trade1 = Trade.create(
   date: "2025-05-08",
-  amount: 550.00
+  amount: 550.00,
+  symbol: "AAPL",
+  trade_type: "Buy",
+  option_type: "",
+  notes: "Test Notes",
+  was_disciplined: true
 )
 
 user1.trades << trade1
